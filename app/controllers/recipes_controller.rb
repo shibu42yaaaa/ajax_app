8792992589faac8a2d38bed7ed885a97bab03e7f
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2022-10-24 11:23:48 +0900

    アクション追加index create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,17 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Resipe.all
+    @recipe = Recipe.new
+  end
+
+  def create
+    Recipe.create(recipe_params)
+    @recipes = Recipe.all
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).parmit(:title, :body)
   end
 end
